aa4127ab06cd1c3ce8f8e6c41128b7d77729ca19
clm-whyte <clm.whyte@gmail.com>
2020-06-09 13:15:21 +0100

    render Tic-Tac-Toe board separently from Task List

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
+import Game from "./Game";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />,
+    <div className="sudoku">
+      <Game />
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
