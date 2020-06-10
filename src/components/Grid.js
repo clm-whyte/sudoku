822d5be38a238c923fd0e25e1e3eb679e04c7754
clm-whyte <clm.whyte@gmail.com>
2020-06-10 12:28:54 +0100

    remove the 4x4 grid to make regular tic-tac-toe

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -44,25 +44,16 @@ class Grid extends React.Component {
           {this.renderCell(0)}
           {this.renderCell(1)}
           {this.renderCell(2)}
-          {this.renderCell(3)}
         </div>
         <div className="grid-row">
           {this.renderCell(4)}
           {this.renderCell(5)}
           {this.renderCell(6)}
-          {this.renderCell(7)}
         </div>
         <div className="grid-row">
           {this.renderCell(8)}
           {this.renderCell(9)}
           {this.renderCell(10)}
-          {this.renderCell(11)}
-        </div>
-        <div className="grid-row">
-          {this.renderCell(12)}
-          {this.renderCell(13)}
-          {this.renderCell(14)}
-          {this.renderCell(15)}
         </div>
       </div>
     );
