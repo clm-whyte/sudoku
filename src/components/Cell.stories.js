77f58448c65be7bc3d987be57ce93a48787a7fa9
clm-whyte <clm.whyte@gmail.com>
2020-06-11 17:30:42 +0100

    build up default cell in storybook

---
+++ src/components/Cell.stories.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs/react";
+import { withA11y } from "@storybook/addon-a11y";
+
+import Cell from "./Cell";
+
+export default {
+  component: Cell,
+  title: "Cell",
+  decorators: [withKnobs, withA11y],
+  excludeStories: /.*Data$/,
+};
+
+export const cellData = {
+  id: "1",
+  value: 1,
+  isSelected: false,
+};
+
+export const actionsData = {
+  onSelectCell: action("onSelectCell"),
+  onEnterDigit: action("onEnterDigit"),
+};
+
+export const Empty = () => {
+  return <Cell cell={object("cell", { ...cellData })} {...actionsData} />;
+};
