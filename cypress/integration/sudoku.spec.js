ab6b6466914f2ec33bfae76ebfe812d5b04533c4
clm-whyte <clm.whyte@gmail.com>
2020-06-19 14:32:49 +0100

    enable aXe accessibility checks as part of cypress

--- cypress/integration/sudoku.spec.js
+++ cypress/integration/sudoku.spec.js
@@ -3,7 +3,12 @@
 describe("Sudoku E2E Tests", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
+    cy.injectAxe();
   });
+  it("Has no detectable a11y violations on load", () => {
+    cy.checkA11y();
+  });
+
   it("verifies that the empty grid is rendered correctly", () => {
     cy.get('[data-test="grid"]').should("have.length", 1);
     cy.get(".cell").should("have.length", 81);
@@ -12,5 +17,6 @@ describe("Sudoku E2E Tests", () => {
   it("clicks the first cell", () => {
     cy.get('[data-test="cell-0/0"]').click();
     cy.get('[data-test="cell-0/0"]').should("have.text", 0);
+    cy.checkA11y();
   });
 });
