73910695959d6970599904b24241d90f2e2500b3
clm-whyte <clm.whyte@gmail.com>
2020-06-22 13:47:15 +0100

    extract all SudokuCell styling into Sass stylesheet

--- src/components/SudokuCell.js
+++ src/components/SudokuCell.js
@@ -1,31 +1,24 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import { StylesProvider } from "@material-ui/core/styles";
 
 export default function SudokuCell(props) {
-  const cellSize = "34px";
-  const cellBorder = "1px solid rgba(0, 0, 0, 0.23)";
-  const boxBorder = "2px solid black";
-
   return (
-    <Button
-      variant="outlined"
-      style={{
-        maxWidth: `${cellSize}`,
-        maxHeight: `${cellSize}`,
-        minWidth: `${cellSize}`,
-        minHeight: `${cellSize}`,
-        border: `${cellBorder}`,
-        borderRadius: 0,
-        borderTop: `${props.row % 3 === 0 ? `${boxBorder}` : `${cellBorder}`}`,
-        borderBottom: `${props.row === 8 ? `${boxBorder}` : `${cellBorder}`}`,
-        borderLeft: `${props.col % 3 === 0 ? `${boxBorder}` : `${cellBorder}`}`,
-        borderRight: `${props.col === 8 ? `${boxBorder}` : `${cellBorder}`}`,
-      }}
-      aria-label={props.cellID}
-      data-test={props.cellID}
-      onClick={props.onClick}
-    >
-      {props.value}
-    </Button>
+    <StylesProvider injectFirst>
+      <Button
+        variant="outlined"
+        aria-label={props.cellID}
+        data-test={props.cellID}
+        onClick={props.onClick}
+        className={`sudokuCell
+        ${props.row % 3 === 0 ? "boxBoundaryTop" : ""}
+        ${props.row === 8 ? "boxBoundaryBottom" : ""}
+        ${props.col % 3 === 0 ? "boxBoundaryLeft" : ""}
+        ${props.col === 8 ? "boxBoundaryRight" : ""}
+        `}
+      >
+        {props.value}
+      </Button>
+    </StylesProvider>
   );
 }
