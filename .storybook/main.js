189e89359d211b379371f77f86928495b6a3f9a5
clm-whyte <clm.whyte@gmail.com>
2020-06-29 10:27:03 +0100

    install Storybook Docs addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
-  stories: ["../src/components/**/*.stories.js"],
+  stories: ["../src/components/**/*.stories.@(js|mdx)"],
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
     "@storybook/addon-knobs",
     "@storybook/addon-a11y/register",
     "@storybook/addon-links",
+    "@storybook/addon-docs",
   ],
 };
