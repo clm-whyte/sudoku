9f4d84e726033f37d9e652f3d84ab22e9d21e9b5
clm-whyte <clm.whyte@gmail.com>
2020-07-01 10:22:28 +0100

    working MDX document for Cell Test

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,19 @@
 module.exports = {
-  stories: ["../src/components/**/*.stories.js"],
+  stories: ["../src/**/*.stories.(js|mdx)"],
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
     "@storybook/addon-knobs",
-    "@storybook/addon-a11y/register",
+    "@storybook/addon-a11y",
     "@storybook/addon-links",
+    "@storybook/addon-storysource",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+      },
+    },
   ],
 };
