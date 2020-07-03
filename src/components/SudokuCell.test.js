a8f04784d1821c3918206e095b5b5b8602e38a2a
clm-whyte <clm.whyte@gmail.com>
2020-07-03 18:50:26 +0100

    add tests to cover Cell borders for the corner cells

--- src/components/SudokuCell.test.js
+++ src/components/SudokuCell.test.js
@@ -3,7 +3,35 @@ import React from "react";
 import { shallow } from "enzyme";
 import SudokuCell from "./SudokuCell";
 
-test("test shallow render of Sudoku Cell", () => {
-  const sudokuCell = shallow(<SudokuCell />);
-  sudokuCell.unmount();
+describe("Cell borders", () => {
+  const gridTopLeft = shallow(<SudokuCell row="0" col="0" />);
+  const gridTopRight = shallow(<SudokuCell row="0" col="8" />);
+  const gridBotLeft = shallow(<SudokuCell row="8" col="0" />);
+  const gridBotRight = shallow(<SudokuCell row="8" col="8" />);
+
+  it("should match the snapshot", () => {
+    expect(gridTopLeft).toMatchSnapshot();
+  });
+
+  it("asserts that the cell in the top left should have a thick border on the top and left", () => {
+    expect(gridTopLeft.html()).toContain("boxBoundaryLeft", "boxBoundaryTop");
+  });
+
+  it("asserts that the cell in the top right should have a thick border on the top and right", () => {
+    expect(gridTopRight.html()).toContain("boxBoundaryRight", "boxBoundaryTop");
+  });
+
+  it("asserts that the cell in the bottom left should have a thick border on the bottom and left", () => {
+    expect(gridBotLeft.html()).toContain(
+      "boxBoundaryLeft",
+      "boxBoundaryBottom"
+    );
+  });
+
+  it("asserts that the cell in the top right should have a thick border on the top and right", () => {
+    expect(gridBotRight.html()).toContain(
+      "boxBoundaryRight",
+      "boxBoundaryBottom"
+    );
+  });
 });
