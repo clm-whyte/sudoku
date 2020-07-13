b9bd54897ac235cce4571c70aef5e2ca5e03a754
clm-whyte <clm.whyte@gmail.com>
2020-07-13 16:12:01 +0100

    refactor Cypress shift click tests to reduce number of tests required

--- cypress/integration/sudoku.spec.js
+++ cypress/integration/sudoku.spec.js
@@ -39,33 +39,61 @@ describe("Sudoku E2E Tests", () => {
     cy.get('[data-test="cell-0/8"]').type("{ctrl}", { release: false }).click();
     cy.get('[data-test="cell-8/0"]').type("{ctrl}", { release: false }).click();
     cy.get(".selected").should("have.length", 3);
+    cy.checkA11y();
   });
 
-  it("clicks one cell, and then SHIFT + Clicks a cell in the middle of the same row and then at the end of the same row", () => {
-    cy.get('[data-test="cell-0/0"]')
+  it("clicks the centre cell, then SHIFT+Clicks an outer cell in each cardinal direction", () => {
+    cy.get('[data-test="cell-0/0"]').should("have.class", "cursor");
+
+    cy.get('[data-test="cell-4/4"]')
       .click()
       .type("{shift}", { release: false });
+    cy.get('[data-test="cell-4/4"]').should("have.class", "cursor");
+
     cy.get('[data-test="cell-0/4"]')
       .type("{shift}", { release: false })
       .click();
+    cy.get('[data-test="cell-4/4"]').should("have.class", "cursor");
     cy.get(".selected").should("have.length", 5);
-    cy.get('[data-test="cell-0/8"]')
+    cy.get('[data-test="cell-0/4"]').should("have.class", "selected");
+    cy.get('[data-test="cell-1/4"]').should("have.class", "selected");
+    cy.get('[data-test="cell-2/4"]').should("have.class", "selected");
+    cy.get('[data-test="cell-3/4"]').should("have.class", "selected");
+    cy.get('[data-test="cell-4/4"]').should("have.class", "selected");
+
+    cy.get('[data-test="cell-4/8"]')
       .type("{shift}", { release: false })
       .click();
-    cy.get(".selected").should("have.length", 9);
-  });
+    cy.get('[data-test="cell-4/4"]').should("have.class", "cursor");
+    cy.get(".selected").should("have.length", 5);
+    cy.get('[data-test="cell-4/4"]').should("have.class", "selected");
+    cy.get('[data-test="cell-4/5"]').should("have.class", "selected");
+    cy.get('[data-test="cell-4/6"]').should("have.class", "selected");
+    cy.get('[data-test="cell-4/7"]').should("have.class", "selected");
+    cy.get('[data-test="cell-4/8"]').should("have.class", "selected");
 
-  // it("clicks one cell, and then SHIFT + Clicks a cell in the middle of the same column and then at the end of the same column", () => {
-  //   cy.get('[data-test="cell-0/0"]')
-  //     .click()
-  //     .type("{shift}", { release: false });
-  //   cy.get('[data-test="cell-4/0"]')
-  //     .type("{shift}", { release: false })
-  //     .click();
-  //   cy.get('[data-test="cell-8/0"]')
-  //     .type("{shift}", { release: false })
-  //     .click();
-  //   cy.get(".sudokuCell").should("have.text", "03672");
-  //   cy.get(".selected").should("have.length", 9);
-  // });
+    cy.get('[data-test="cell-8/4"]')
+      .type("{shift}", { release: false })
+      .click();
+    cy.get('[data-test="cell-4/4"]').should("have.class", "cursor");
+    cy.get(".selected").should("have.length", 5);
+    cy.get('[data-test="cell-4/4"]').should("have.class", "selected");
+    cy.get('[data-test="cell-5/4"]').should("have.class", "selected");
+    cy.get('[data-test="cell-6/4"]').should("have.class", "selected");
+    cy.get('[data-test="cell-7/4"]').should("have.class", "selected");
+    cy.get('[data-test="cell-8/4"]').should("have.class", "selected");
+
+    cy.get('[data-test="cell-4/0"]')
+      .type("{shift}", { release: false })
+      .click();
+    cy.get('[data-test="cell-4/4"]').should("have.class", "cursor");
+    cy.get(".selected").should("have.length", 5);
+    cy.get('[data-test="cell-4/0"]').should("have.class", "selected");
+    cy.get('[data-test="cell-4/1"]').should("have.class", "selected");
+    cy.get('[data-test="cell-4/2"]').should("have.class", "selected");
+    cy.get('[data-test="cell-4/3"]').should("have.class", "selected");
+    cy.get('[data-test="cell-4/4"]').should("have.class", "selected");
+
+    cy.checkA11y();
+  });
 });
