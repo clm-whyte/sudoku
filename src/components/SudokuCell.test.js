6222a17c5033a77772b1835ad72ec1e535d0fe18
clm-whyte <clm.whyte@gmail.com>
2020-07-14 15:33:23 +0100

    update SudokuCell Jest tests

--- src/components/SudokuCell.test.js
+++ src/components/SudokuCell.test.js
@@ -3,36 +3,48 @@ import React from "react";
 import { shallow } from "enzyme";
 import SudokuCell from "./SudokuCell";
 
-describe("Cell borders", () => {
-  it("asserts that the cell in the top left should have a thick border on the top and left", () => {
+describe("Cells have different borders dependant on location", () => {
+  it("cell in top left has thick border on top and left", () => {
     const cell = shallow(<SudokuCell row="0" col="0" />);
     expect(cell.html()).toContain("boxBoundaryLeft", "boxBoundaryTop");
     cell.unmount();
   });
 
-  it("asserts that the cell in the top right should have a thick border on the top and right", () => {
+  it("cell in top right has thick border on top and right", () => {
     const cell = shallow(<SudokuCell row="0" col="8" />);
     expect(cell.html()).toContain("boxBoundaryRight", "boxBoundaryTop");
     cell.unmount();
   });
 
-  it("asserts that the cell in the bottom left should have a thick border on the bottom and left", () => {
+  it("cell in bottom left has thick border on bottom and left", () => {
     const cell = shallow(<SudokuCell row="8" col="0" />);
     expect(cell.html()).toContain("boxBoundaryLeft", "boxBoundaryBottom");
     cell.unmount();
   });
 
-  it("asserts that the cell in the top right should have a thick border on the top and right", () => {
+  it("cell top right has thick border on top and right", () => {
     const cell = shallow(<SudokuCell row="8" col="8" />);
     expect(cell.html()).toContain("boxBoundaryRight", "boxBoundaryBottom");
     cell.unmount();
   });
 });
 
-describe("Cell selection", () => {
-  it("asserts asserts that when passed the prop 'selected=true', the rendered html contains the class 'selected'", () => {
+describe("Cells are rendered with different classes based on which props are passed", () => {
+  it("prop 'selected=true' renders class 'selected'", () => {
     const cell = shallow(<SudokuCell selected="true" />);
     expect(cell.html()).toContain("selected");
+    cell.unmount();
+  });
+
+  it("prop 'cursor=true' renders class 'cursor'", () => {
+    const cell = shallow(<SudokuCell cursor="true" />);
+    expect(cell.html()).toContain("cursor");
+    cell.unmount();
+  });
+
+  it("prop 'cursor=true' and 'selected=true' renders both 'cursor' and 'selected'", () => {
+    const cell = shallow(<SudokuCell cursor="true" selected="true" />);
+    expect(cell.html()).toContain("cursor", "selected");
     cell.unmount();
   });
 });
